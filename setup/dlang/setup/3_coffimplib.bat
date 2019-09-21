6d5b0d8cbbf5c79a7244f1a7edb169ab3e6813cd
karanokuri <karanokuri@gmail.com>
2019-09-21 10:27:17 +0900

    add setup dlang

---
+++ setup/dlang/setup/3_coffimplib.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+pushd %~dp0
+
+set ARCHIVE=coffimplib.zip
+set URL=http://ftp.digitalmars.com/%ARCHIVE%
+
+set DIST=%KARANOENV_BIN_DIR%
+
+if exist %DIST%\coffimplib.exe (
+  echo coffimplib is already exists.
+  exit /b 0
+)
+
+if not exist %DIST% mkdir %DIST%
+
+echo Downloading %URL% ...
+powershell -c "(new-object net.webclient).DownloadFile('%URL%','%ARCHIVE%')"
+
+busybox unzip -oq %ARCHIVE% -d %DIST%
+
+del %ARCHIVE%
+
+popd
+endlocal
