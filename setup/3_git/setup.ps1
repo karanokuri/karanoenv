bd2aa849133676ef4d115237fec9f67e2b23b95a
karanokuri <karanokuri@gmail.com>
2019-09-21 11:20:06 +0900

    add dotfiles related git

--- setup/3_git/setup.ps1
+++ setup/3_git/setup.ps1
@@ -3,29 +3,40 @@ $ErrorActionPreference = "Stop"
 
 $API_URL = "https://api.github.com/repos/git-for-windows/git/releases/latest"
 
-$ARCHIVE = "PortableGit-$($Env:KARANOENV_ARCH)-bit.7z.exe"
+$ARCH = $Env:KARANOENV_ARCH
+
+$ARCHIVE = "PortableGit.7z.exe"
 
 $DIST = Join-Path $Env:KARANOENV_APPS_DIR "PortableGit"
 
 ###############################################################################
-if(Test-Path $DIST)
+if(!(Test-Path $DIST))
 {
-  Write-Host "PortableGit is already exists."
-  exit 0
+  $Url = Invoke-RestMethod -Uri $API_URL -Method GET           |
+           % assets                                            |
+           ?{ $_.name -like "PortableGit-*-$ARCH-bit.7z.exe" } |
+           % browser_download_url
+
+  Write-Host "Downloading $URL ..."
+  (new-object net.webclient).DownloadFile($Url, $ARCHIVE)
+
+  if(!(Test-Path($DIST))){ mkdir $DIST | Out-Null }
+
+  Write-Host "Extracting $ARCHIVE ..."
+  7z x -y $ARCHIVE "-o$DIST"
+  & (Join-Path $DIST "post-install.bat")
+
+  del $ARCHIVE
 }
 
-$Url = Invoke-RestMethod -Uri $API_URL -Method GET                            |
-         % assets                                                             |
-         ?{ $_.name -like "PortableGit-*-$($Env:KARANOENV_ARCH)-bit.7z.exe" } |
-         % browser_download_url
+git config --global core.excludesfile | Out-Null
+if(!($?))
+{
+  git config --global core.excludesfile ~/.karanoenv/dotfiles/.gitignore
+}
 
-Write-Host "Downloading $URL ..."
-(new-object net.webclient).DownloadFile($Url, $ARCHIVE)
-
-if(!(Test-Path($DIST))){ mkdir $DIST | Out-Null }
-
-Write-Host "Extracting $ARCHIVE ..."
-7z x -y $ARCHIVE "-o$DIST"
-& (Join-Path $DIST "post-install.bat")
-
-del $ARCHIVE
+git config --global include.path | Out-Null
+if(!($?))
+{
+  git config --global include.path ~/.karanoenv/dotfiles/.gitconfig
+}
