4af77038adb55f3382e623a452bf80022de16fe5
karanokuri <karanokuri@gmail.com>
2020-08-10 16:15:46 +0900

    fix setup dscanner

--- setup/dlang/setup/dscanner.ps1
+++ setup/dlang/setup/dscanner.ps1
@@ -1,25 +1,32 @@
 $ErrorActionPreference = "Stop"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$API_URL = "https://api.github.com/repos/dlang-community/D-Scanner/releases/latest"
+
+$ARCH = if($Env:KARANOENV_ARCH -eq '64'){'x86_64'}else{'x86'}
+
+$ARCHIVE = "dscanner.zip"
 
 $DIST = $Env:KARANOENV_BIN_DIR
 
+###############################################################################
 if(Join-Path $DIST dscanner.exe | Test-Path)
 {
   Write-Host "dscanner is already exists."
   exit 0
 }
 
-dub fetch dscanner
-dub build -a x86 -b release dscanner
-$packageInfo = dub describe dscanner         |
-                 ConvertFrom-Json            |
-                 % packages                  |
-                 ?{ $_.name -eq "dscanner" }
+$Url = Invoke-RestMethod -Uri $API_URL -Method GET                  |
+         % assets                                                   |
+         ?{ $_.name -match "dscanner-v[0-9.]+-windows-$ARCH\.zip" } |
+         % browser_download_url
 
-$baseDir        = $packageInfo | % path
-$targetDir      = $packageInfo | % targetPath
-$targetFileName = $packageInfo | % targetFileName
+Write-Host "Downloading $URL ..."
+(new-object net.webclient).DownloadFile($Url, $ARCHIVE)
 
-$targetPath = Join-Path $baseDir $targetDir        |
-              Join-Path -ChildPath $targetFileName
+if(!(Test-Path($DIST))){ mkdir $DIST | Out-Null }
 
-copy $targetPath $DIST
+Write-Host "Extracting $ARCHIVE ..."
+7z e -y $ARCHIVE "-o$DIST" '-ir!dscanner.exe'
+
+del $ARCHIVE
