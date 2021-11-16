670808b67d263a4fcb9cc486e2d79e79d0ff91bb
karanokuri <karanokuri@gmail.com>
2021-11-16 22:39:51 +0900

    fix setup pyenv

--- setup/5_python/setup/0_pyenv.bat
+++ setup/5_python/setup/0_pyenv.bat
@@ -6,6 +6,7 @@ if not exist .git (
   git init
   git remote add origin https://github.com/pyenv-win/pyenv-win.git
 )
+del pyenv-win\.versions_cache.xml 2>nul
 git pull --set-upstream origin master
 popd
 
