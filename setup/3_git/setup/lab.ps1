accb2d7c2b2be679baa8b752f18dd15f3c6771ba
karanokuri <karanokuri@gmail.com>
2022-01-06 14:44:34 +0900

    update setup lab

--- setup/3_git/setup/lab.ps1
+++ setup/3_git/setup/lab.ps1
@@ -3,30 +3,37 @@ $ErrorActionPreference = 'Stop'
 
 $API_URL = 'https://api.github.com/repos/zaquestion/lab/releases/latest'
 
-$ARCH = if($Env:KARANOENV_ARCH -eq '64'){'amd64'}else{'386'}
-
-$ARCHIVE = 'lab.tar.gz'
+$ARCH = if($Env:KARANOENV_ARCH -eq '64')
+{
+  'amd64'
+} else
+{
+  '386'
+}
 
 $DIST = $Env:KARANOENV_BIN_DIR
 
 ###############################################################################
-if(Join-Path $DIST 'lab.exe' | Test-Path)
+$Version = cmd /c 'lab --version 2>nul'            `
+| Where-Object{ $_ -like 'lab version *' }         `
+| ForEach-Object{ $_.replace('lab version ', '') } #
+
+$Latest = Invoke-RestMethod -Uri $API_URL -Method GET
+
+if ("v$Version" -eq $Latest.tag_name)
 {
-  Write-Host "lab already exists."
+  Write-Host "lab is already latest."
   exit 0
 }
 
-$Url = Invoke-RestMethod -Uri $API_URL -Method GET              |
-         % assets                                               |
-         ?{ $_.name -match "lab_[0-9.]+_windows_$ARCH.tar.gz" } |
-         % browser_download_url
+if(!(Test-Path $DIST))
+{
+  mkdir $DIST | Out-Null
+}
 
-Write-Host "Downloading $URL ..."
-(new-object net.webclient).DownloadFile($Url, $ARCHIVE)
+$Url = $Latest.assets                                               `
+| Where-Object{ $_.name -match "lab_[0-9.]+_windows_$ARCH.tar.gz" } `
+| ForEach-Object browser_download_url                               #
 
-if(!(Test-Path($DIST))){ mkdir $DIST | Out-Null }
-
-Write-Host "Extracting $ARCHIVE ..."
-tar zxf $ARCHIVE -C $DIST lab.exe
-
-del $ARCHIVE
+Write-Host "Installing $Url ..."
+cmd /c "%WINDIR%\system32\curl.exe -sSL $Url | %WINDIR%\system32\tar.exe -xJf - -C $DIST lab.exe"
