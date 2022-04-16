885677a2a9fa6dea5f1cb9dca5ad95b2251352d8
karanokuri <karanokuri@gmail.com>
2022-04-17 04:45:59 +0900

    fix setup python

--- setup/5_python/setup/1_python.bat
+++ setup/5_python/setup/1_python.bat
@@ -1,9 +1,9 @@
 @echo off
 
 if "%KARANOENV_ARCH%"=="64" (
-  for /f %%V in ('pyenv install --list ^| findstr /r "^2.*\.[0-9]*$" ^| tail -n 1') do cmd /c "pyenv install %%V"
-  for /f %%V in ('pyenv install --list ^| findstr /r "^3.*\.[0-9]*$" ^| tail -n 1') do cmd /c "pyenv install %%V"
+  for /f %%V in ('pyenv install --list ^| findstr /r "^2.*\.[0-9]*$" ^| tail -n 1') do cmd /c "pyenv install -q %%V"
+  for /f %%V in ('pyenv install --list ^| findstr /r "^3.*\.[0-9]*$" ^| tail -n 1') do cmd /c "pyenv install -q %%V"
 ) else (
-  for /f %%V in ('pyenv install --list ^| findstr /r "^2.*\.[0-9]*-win32$" ^| tail -n 1') do cmd /c "pyenv install %%V"
-  for /f %%V in ('pyenv install --list ^| findstr /r "^3.*\.[0-9]*-win32$" ^| tail -n 1') do cmd /c "pyenv install %%V"
+  for /f %%V in ('pyenv install --list ^| findstr /r "^2.*\.[0-9]*-win32$" ^| tail -n 1') do cmd /c "pyenv install -q %%V"
+  for /f %%V in ('pyenv install --list ^| findstr /r "^3.*\.[0-9]*-win32$" ^| tail -n 1') do cmd /c "pyenv install -q %%V"
 )
