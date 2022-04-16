b2b10554444d359762925b131e7414f1e0860a40
karanokuri <karanokuri@gmail.com>
2022-04-17 04:37:21 +0900

    fix setup gcloud

--- setup/gcloud/setup.ps1
+++ setup/gcloud/setup.ps1
@@ -1,31 +1,40 @@
 $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$ARCH = if($Env:KARANOENV_ARCH -eq 64) { 'x86_64' } else { 'x86' }
+$ARCH = if($Env:KARANOENV_ARCH -eq 64)
+{
+  'x86_64'
+} else
+{
+  'x86'
+}
 
 $ARCHIVE = "gcloud.zip"
 
 $DIST = $Env:KARANOENV_APPS_DIR
 
 ###############################################################################
-$Version = cmd /c 'gcloud --version 2>nul'            |
-             ?{ $_ -like 'Google Cloud SDK*' }        |
-             %{ $_.replace('Google Cloud SDK ', '') } #
+$Version = cmd /c 'gcloud --version 2>nul'              `
+| Where-Object{ $_ -like 'Google Cloud SDK*' }          `
+| ForEach-Object{ $_.replace('Google Cloud SDK ', '') } #
 
 $PageUrl = 'https://cloud.google.com/sdk/docs/downloads-versioned-archives'
-$ParsedHtml = Invoke-WebRequest -Uri $PageUrl | % ParsedHtml
-$Url = $ParsedHtml.getElementsByTagName('a')                              |
-         % href                                                           |
-         ?{ $_ -match "google-cloud-sdk-([\d.]+)-windows-$ARCH-bundled" } |
-         select -First 1                                                  #
+$Url = Invoke-WebRequest -Uri $PageUrl                                        `
+| ForEach-Object{ $_.Links }                                                  `
+| ForEach-Object href                                                         `
+| Where-Object{ $_ -match "google-cloud-sdk-([\d.]+)-windows-$ARCH-bundled" } `
+| Select-Object -First 1                                                      #
 
-if($Url -notlike "*-$Version-*")
+if($Url -like "*-$Version-*")
 {
-  Write-Host "Downloading $Url ..."
-  (new-object net.webclient).DownloadFile($Url, $ARCHIVE)
-
-  Write-Host "Extracting $ARCHIVE ..."
-  & 7z x -y $ARCHIVE "-o$DIST"
-
-  del $ARCHIVE
+  Write-Host 'gcloud is already latest.'
+  exit 0
 }
+
+Write-Host "Downloading $Url ..."
+(new-object net.webclient).DownloadFile($Url, $ARCHIVE)
+
+Write-Host "Extracting $ARCHIVE ..."
+& 7z x -y $ARCHIVE "-o$DIST"
+
+Remove-Item $ARCHIVE
