969fd3435b0cebd4b9706e3bdf25f23d3b17c7a0
karanokuri <karanokuri@gmail.com>
2022-04-17 04:35:37 +0900

    update setup gnupg

--- setup/gnupg/setup.ps1
+++ setup/gnupg/setup.ps1
@@ -8,25 +8,32 @@ $ARCHIVE = 'gnupg-w32.exe'
 $DIST = Join-Path $Env:KARANOENV_APPS_DIR 'gnupg'
 
 ###############################################################################
-if(Test-Path $DIST)
+$Version = cmd /c 'gpg --version 2>nul'            `
+| Where-Object{ $_ -like 'gpg (GnuPG) *' }         `
+| ForEach-Object{ $_.replace('gpg (GnuPG) ', '') } #
+
+$Path = Invoke-WebRequest -Uri $PAGE_URL                   `
+| ForEach-Object{ $_.Links }                               `
+| ForEach-Object href                                      `
+| Where-Object{ $_ -match '/gnupg-w32-[0-9.]+_\d+\.exe$' } #
+
+if($Path -like "*-${Version}_*")
 {
-  Write-Host 'gnupg is already exists.'
+  Write-Host 'gnupg is already latest.'
   exit 0
 }
-
-$Path = Invoke-WebRequest -Uri $PAGE_URL                |
-          %{ $_.Links }                                 |
-          % href                                        |
-          ?{ $_ -match '/gnupg-w32-[0-9.]+_\d+\.exe$' }
 
 $Url = New-Object Uri($PAGE_URL, $Path)
 
 Write-Host "Downloading $Url ..."
 (new-object net.webclient).DownloadFile($Url, $ARCHIVE)
 
-if(!(Test-Path($DIST))){ mkdir $DIST | Out-Null }
-
 Write-Host "Extracting $ARCHIVE ..."
+if(Test-Path $DIST)
+{
+  Remove-Item $DIST -Recurse -Force
+}
+mkdir $DIST | Out-Null
 7z x -y $ARCHIVE "-o$DIST" '-xr!$*' '-xr!gnupg-uninstall.exe.nsis'
 
-del $ARCHIVE
+Remove-Item $ARCHIVE
