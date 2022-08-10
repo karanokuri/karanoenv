b785f77d2b6f00e16ddb42e8bfd8842451060755
karanokuri <karanokuri@gmail.com>
2022-08-11 00:10:18 +0900

    fix install.ps1

--- install.ps1
+++ install.ps1
@@ -1,40 +1,52 @@
 $ErrorActionPreference = "Stop"
 
-$REPO_URL = "https://github.com/karanokuri/karanoenv"
-$DIST     = Join-Path $Env:USERPROFILE '.karanoenv'
+$REPO_URL    = 'https://github.com/karanokuri/karanoenv'
+$BRANCH_NAME = 'main'
+$DIST        = Join-Path $Env:USERPROFILE '.karanoenv'
 
 if(Test-Path $DIST)
 {
-  Write-Host "karanoenv is already installed"
+  Write-Host 'karanoenv is already installed'
   if($MyInvocation.MyCommand.CommandType -eq 'Script')
   {
     return
-  }
-  else
+  } else
   {
     exit 1
   }
 }
 
-$Url = "$REPO_URL/archive/master.zip"
-$TmpDir = New-TemporaryFile | %{ rm $_; mkdir $_ }
-$TmpZip = "$TmpDir\karanoenv.zip"
+$Url = "$REPO_URL/archive/refs/heads/$BRANCH_NAME.zip"
+$TmpDir = New-TemporaryFile | ForEach-Object{ Remove-Item $_; mkdir $_ }
+$TmpZip = Join-Path $TmpDir "karanoenv.zip"
 
-if(!(Test-Path $DIST)) { mkdir $DIST | Out-Null }
+if(!(Test-Path $DIST))
+{
+  mkdir $DIST | Out-Null
+}
 
 Write-Host 'Downloading ...'
 (new-object net.webclient).DownloadFile($Url, $TmpZip)
 
 Write-Host 'Extracting ...'
 Expand-Archive $TmpZip $TmpDir
-cp "$TmpDir\*master\*" $DIST -Recurse -Force
+Copy-Item "$TmpDir\*$BRANCH_NAME\*" $DIST -Recurse -Force
+Remove-Item $TmpDir -Recurse -Force
 
-pushd $DIST
-  $Clone = "$TmpDir\karanoenv"
+Push-Location $DIST
+try
+{
+  .\setup.ps1
 
-  & .\setup.bat
-  cmd /c "call setenv.bat && git clone -q $REPO_URL.git $Clone"
-  cp "$Clone\*" '.' -Recurse -Force
-popd
+  cmd /c ".\setenv.bat && set" | ForEach-Object {
+    $EnvName, $EnvValue = $_.split('=')
+    Set-Item -Path (Join-Path Env:\ $EnvName) -Value $EnvValue
+  }
 
-rmdir $TmpDir -Recurse -Force
+  git init
+  git remote add origin $REPO_URL
+  git fetch origin $BRANCH_NAME
+} finally
+{
+  Pop-Location
+}
