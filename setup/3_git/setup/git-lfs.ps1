2f90b4e164ff2d88a7da53a4a41ee18aaa1e39bc
karanokuri <karanokuri@gmail.com>
2022-08-19 19:02:14 +0900

    fix setup git-lfs

--- setup/3_git/setup/git-lfs.ps1
+++ setup/3_git/setup/git-lfs.ps1
@@ -3,21 +3,38 @@ $ErrorActionPreference = "Stop"
 
 $API_URL = "https://api.github.com/repos/git-lfs/git-lfs/releases/latest"
 
-$ARCH = if($Env:KARANOENV_ARCH -eq '64'){'amd64'}else{'386'}
+$ARCH = if($Env:KARANOENV_ARCH -eq '64')
+{
+  'amd64'
+} else
+{
+  '386'
+}
+
+$ARCHIVE = "git-lfs.zip"
 
 $DIST = $Env:KARANOENV_BIN_DIR
 
 ###############################################################################
-if(Join-Path $DIST 'git-lfs.exe' | Test-Path)
+$Version = cmd /c 'git-lfs version 2>nul'                     `
+| ForEach-Object{ $_ -replace '^git-lfs/([0-9._]+)*$', '$1' } #
+
+$Latest = Invoke-RestMethod -Uri $API_URL -Method GET
+
+if ("v$Version" -eq $Latest.tag_name)
 {
-  Write-Host "git-lfs already exists."
+  Write-Host "git-lfs is already latest."
   exit 0
 }
 
-$Url = Invoke-RestMethod -Uri $API_URL -Method GET           |
-         % assets                                            |
-         ? name -match "git-lfs-windows-$ARCH-v[0-9.]+\.zip" |
-         % browser_download_url
+$Url = $Latest.assets                                            `
+| Where-Object name -match "git-lfs-windows-$ARCH-v[0-9.]+\.zip" `
+| ForEach-Object browser_download_url                            #
 
-Write-Host "Installing $Url ..."
-cmd /c "wget -O - $Url | busybox unzip - git-lfs.exe -d $DIST"
+Write-Host "Downloading $Url ..."
+(new-object net.webclient).DownloadFile($Url, $ARCHIVE)
+
+Write-Host "Extracting $ARCHIVE ..."
+7z e -y $ARCHIVE "-ir!git-lfs.exe" "-o$DIST" | Out-Null
+
+Remove-Item $ARCHIVE
