5a81b1b87fa4fdf8bd4441fdc09f7533ed517994
karanokuri <karanokuri@gmail.com>
2022-09-14 10:51:35 +0900

    update dotfiles/.wezterm.lua

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
+local mux = wezterm.mux
 
-wezterm.add_to_config_reload_watch_list(wezterm.home_dir..'/.karanoenv/dotfiles/.wezterm.lua')
+wezterm.add_to_config_reload_watch_list(wezterm.home_dir .. '/.karanoenv/dotfiles/.wezterm.lua')
 
 local function file_exists(name)
-  local f = io.open(name,"r")
+  local f = io.open(name, "r")
   if f == nil then
     return false
   end
@@ -19,6 +20,14 @@ local function load_local_config(file)
   return {}
 end
 
+local default_cwd
+
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+  default_cwd = config.default_cwd
+  local cwd = tab.active_pane.current_working_dir:sub(9)
+  return cwd
+end)
+
 wezterm.on(
   'format-tab-title',
   function(tab, tabs, panes, config, hover, max_width)
@@ -29,6 +38,20 @@ wezterm.on(
   end
 )
 
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+wezterm.on('spawn-new-tab', function(window, pane)
+  window:perform_action(
+    act.SpawnCommandInNewTab {
+      cwd = default_cwd,
+    },
+    pane
+  )
+end)
+
 local config = {
   colors = {
     cursor_fg = 'black',
@@ -38,20 +61,20 @@ local config = {
   default_prog = { 'cmd.exe', '/c', 'call %USERPROFILE%\\.karanoenv\\setenv.bat && nyagos' },
 
   font = wezterm.font 'HackGen Console NF',
-  font_dirs = { wezterm.home_dir..'/.karanoenv/apps/fonts' },
+  font_dirs = { wezterm.home_dir .. '/.karanoenv/apps/fonts' },
   font_size = 12.0,
 
   keys = {
-    { key = 'n', mods = 'CTRL', action = act.SpawnCommandInNewTab {} },
+    { key = 'n', mods = 'CTRL', action = act.EmitEvent 'spawn-new-tab' },
 
-    { key = 'h',          mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Left'  },
-    { key = 'l',          mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Right' },
-    { key = 'k',          mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Up'    },
-    { key = 'j',          mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Down'  },
-    { key = 'LeftArrow',  mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Left'  },
+    { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Left' },
+    { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Right' },
+    { key = 'k', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Up' },
+    { key = 'j', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Down' },
+    { key = 'LeftArrow', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Left' },
     { key = 'RightArrow', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Right' },
-    { key = 'UpArrow',    mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Up'    },
-    { key = 'DownArrow',  mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Down'  },
+    { key = 'UpArrow', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Up' },
+    { key = 'DownArrow', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Down' },
   },
 
   use_ime = true,
@@ -60,7 +83,7 @@ local config = {
   window_decorations = 'RESIZE',
 }
 
-for k, v in pairs(load_local_config(wezterm.home_dir..'/.wezterm.lua')) do
+for k, v in pairs(load_local_config(wezterm.home_dir .. '/.wezterm.lua')) do
   config[k] = v
 end
 
