1ea16170868872c8a77e0fe3d4409334593fa45c
karanokuri <karanokuri@gmail.com>
2023-01-06 21:40:28 +0900

    fix setup dub

--- setup/dlang/setup/dub.ps1
+++ setup/dlang/setup/dub.ps1
@@ -1,10 +1,16 @@
 $ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$ARCH = if($Env:KARANOENV_ARCH -eq "64"){"x86_64"}else{"x86"}
+$ARCH = if($Env:KARANOENV_ARCH -eq "64")
+{
+  "x86_64"
+} else
+{
+  "i686"
+}
 
-$API_URL = "https://api.github.com/repos/dlang/dub/releases/latest"
-
+$VERSION = "1.29.0"
+$ARCHIVE_URL = "https://github.com/dlang/dub/releases/download/v$VERSION/dub-v$VERSION-windows-$ARCH.zip"
 $ARCHIVE = "dub-windows.zip"
 
 $DIST = $Env:KARANOENV_BIN_DIR
@@ -16,17 +22,15 @@ if(Join-Path $DIST dub.exe | Test-Path)
   exit 0
 }
 
-$Url = Invoke-RestMethod -Uri $API_URL -Method GET            |
-         % assets                                             |
-         ?{ $_.name -match "dub-v[0-9.]+-windows-$ARCH.zip" } |
-         % browser_download_url
+Write-Host "Downloading $ARCHIVE_URL ..."
+(New-Object Net.WebClient).DownloadFile($ARCHIVE_URL, $ARCHIVE)
 
-Write-Host "Downloading $URL ..."
-(new-object net.webclient).DownloadFile($Url, $ARCHIVE)
-
-if(!(Test-Path($DIST))){ mkdir $DIST | Out-Null }
+if(!(Test-Path($DIST)))
+{
+  mkdir $DIST | Out-Null
+}
 
 Write-Host "Extracting $ARCHIVE ..."
 7z x -y $ARCHIVE "-o$DIST"
 
-del $ARCHIVE
+Remove-Item $ARCHIVE
