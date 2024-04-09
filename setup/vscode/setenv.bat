805f3770ff25cf6997b2467f1aa83732584ac44e
karanokuri <karanokuri@gmail.com>
2024-04-09 18:58:24 +0900

    add ms-vscode-remote.remote-containers extension to vscode

--- setup/vscode/setenv.bat
+++ setup/vscode/setenv.bat
@@ -1,3 +1,4 @@
 @echo off
 
 set PATH=%KARANOENV_APPS_DIR%\vscode\bin;%PATH%
+set PATH=%KARANOENV_APPS_DIR%\vscode\data\user-data\User\globalStorage\ms-vscode-remote.remote-containers\cli-bin;%PATH%
