497dce9731afb353fc9b75f415515b481a9f5d5e
karanokuri <karanokuri@gmail.com>
2025-05-05 16:20:41 +0900

    remove setup memo

--- setup/memo/setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-go install github.com/mattn/memo@latest
-
-if not exist "%APPDATA%\memo" mkdir "%APPDATA%\memo"
-if not exist "%APPDATA%\memo\config.toml" (
-  copy /y "%~dp0config.toml" "%APPDATA%\memo\config.toml" >nul
-)
